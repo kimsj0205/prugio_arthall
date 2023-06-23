e55d8039766c908e92509c47f03329198fd0c12a
kimsj0205 <kkeeyy0205@naver.com>
2023-06-23 11:32:17 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// // 화면 높이값을 알아온다.
+// let winW = $(window).height();
+// console.log(winW);
+// // scrollTop()!, scrollTo() width()!, height()!, addClass()!, removeClass()!, next(), children(), parent(), parentNode() fadeIn()!, fadeOut()!, show(), hide(), scrollDown(), scrollUp(), find(), setInterval(), for문, if문
+
+// // dom 구조의 main 요소를 변수 mainEle에 담는다.
+// let mainEle = $('main');
+
+// // dom요소 main에 높이값을 윈도우 높이값으로 대체한다.
+// mainEle.height(winW);
